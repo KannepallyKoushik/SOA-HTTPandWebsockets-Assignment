b438efea69896dd6204473f5add917918d3bd73a
Jesse Korolainen <jesse.korolainen@hotmail.com>
2022-03-23 18:08:47 +0100

    Started implementing Websocket classes.

---
+++ src/main/java/nl/utwente/soa/sampleWebapplication/services/WebsocketEchoProxyService.java
@@ -0,0 +1,12 @@
+package nl.utwente.soa.sampleWebapplication.services;
+
+import javax.websocket.EncodeException;
+import java.io.IOException;
+
+public class WebsocketEchoProxyService implements EchoProxyService {
+
+    @Override
+    public void echo(String message) throws IOException, EncodeException {
+
+    }
+}
